b5a6451b643891ad3424e44836616e284eb8e67f
habibapanna <habibapanna49@gmail.com>
2026-03-08 13:13:55 +0600

    updated link

--- src/components/Components/Service.jsx
+++ src/components/Components/Service.jsx
@@ -236,7 +236,7 @@ const Service = () => {
           </Fade>
           <Fade direction="up">
             <Link
-              to="projects"
+              to="services"
               className="inline-flex items-center gap-2 px-5 py-2 md:px-8 md:py-4 rounded-lg font-medium shadow-lg bg-gradient-to-br from-[#f9fcff] to-[#e0e7ef] text-[#1c2026] text-sm md:text-[20px]"
             >
               <LuArrowDownUp className="text-xl md:text-2xl" />
